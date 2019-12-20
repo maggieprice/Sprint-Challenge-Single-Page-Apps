21c3c543729882795271e006a1062bf239b0118c
Justin Price <mr.price@Justins-iMac.local>
2019-12-20 12:56:13 -0500

    displaying fields (minus image)

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,23 @@
 import React from "react";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return <span>
+    <div>
+  <Card>
+      <CardBody>
+          <CardTitle><h2>Name:{props.x.name}</h2></CardTitle>
+          <CardImg top width="100%" src={props.image} alt="Card image" />
+              <CardSubtitle>
+                  <h3>Status: {props.x.status} | Species:{props.x.species} | Gender: {props.x.gender}</h3> 
+              </CardSubtitle>       
+      </CardBody>
+  </Card>
+</div></span>;
 }
